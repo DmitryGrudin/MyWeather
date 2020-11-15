7929215277a5d2e8c365323a9d48ab6b056d49e0
Dmitry Grudin <gn-dmitriy@mail.ru>
2020-11-15 22:24:45 +0300

    Added the example request

--- app/src/main/java/com/test/myweather/MainActivity.java
+++ app/src/main/java/com/test/myweather/MainActivity.java
@@ -1,6 +1,8 @@
 package com.test.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.accounts.AccountManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        AccountManager am = AccountManager.get(this);
+        Bundle options = new Bundle();
+
+        am.getAuthToken(
+                myAccount_,                     // Account retrieved using getAccountsByType()
+                "Manage your tasks",            // Auth scope
+                options,                        // Authenticator-specific options
+                this,                           // Your activity
+                new OnTokenAcquired(),          // Callback called when a token is successfully acquired
+                new Handler(new OnError()));    // Callback called if an error occurs
+
     }
 }
